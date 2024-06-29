945048c66591b26c24c8b466bbb00c46344aaaf1
amithhh <158270892+amithhh@users.noreply.github.com>
2024-06-29 08:28:25 +0530

    Update error.jsx

--- src/component/error.jsx
+++ src/component/error.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Error() {
   return (
-    <div style={{ backgroundImage:'url(https://freefrontend.com/assets/img/html-funny-404-pages/HTML-404-Error-Page.gif)',height:'100vh'}}>
+    <div style={{ backgroundImage:'url(https://freefrontend.com/assets/img/html-funny-404-pages/HTML-404-Error-Page.gif)',height:'100vh',backgroundSize:'cover'}}>
       
        
       
