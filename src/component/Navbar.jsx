bec5fcce05c90e4f4fd87860ed1232e2f101e256
anamiikarajesh <158269912+anamiikarajesh@users.noreply.github.com>
2024-07-03 15:39:16 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,13 +6,13 @@ class LoginPage extends React.Component {
 
     render() {
         return (
-            <div className="container" style={{backgroundImage:'url(navimg.jpg)',height:'100vh',backgroundSize:'cover'}}>
-                <h1 style={{ textAlign: 'center', color: '#15CDCA', fontWeight: 'bold' ,paddingTop:'20px'}}>Jyothi Engineering College</h1>
+            <div className="container" style={{backgroundColor:'#98FF98',height:'100vh',backgroundSize:'cover'}}>
+                <h1 style={{ textAlign: 'center', color: '#2F4F4F', fontWeight: 'bold' ,paddingTop:'20px'}}>Jyothi Engineering College</h1>
                 <div className="button-container" style={{ textAlign: 'center' , marginTop: '20px' }}>
-                    <button className="student-login"  style={{ backgroundColor: '#87CDCE', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Home</button>
-                    <a href='/login'><button  className="faculty-login"  style={{ backgroundColor: '#87CDCE', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Login</button></a>
-                    <a href='/About'><button  className="about-us"  style={{ backgroundColor:'#87CDCE', marginRight: '10px', padding: '10px 10px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>About Us</button></a>
-                    <button className="home"  style={{ backgroundColor: '#87CDCE', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>More</button>
+                    <button className="student-login"  style={{ backgroundColor: '#ADD8E6', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Home</button>
+                    <a href='/login'><button  className="faculty-login"  style={{ backgroundColor: '#ADD8E6', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Login</button></a>
+                    <a href='/About'><button  className="about-us"  style={{ backgroundColor:'#ADD8E6', marginRight: '10px', padding: '10px 10px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>About Us</button></a>
+                    <button className="home"  style={{ backgroundColor: '#ADD8E6', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>More</button>
                 </div>
             </div>
         );
