ebb5906e31ad08239134d00f033b37b7cc5954ee
anamiikarajesh <158269912+anamiikarajesh@users.noreply.github.com>
2024-07-03 16:38:19 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -7,12 +7,12 @@ class LoginPage extends React.Component {
     render() {
         return (
             <div className="container" style={{backgroundImage:'url(main.jpg)',height:'100vh',backgroundSize:'cover'}}>
-                <h1 style={{ textAlign: 'center', color: 'white', fontWeight: 'bold' ,paddingTop:'20px'}}>Beyond the Crest</h1>
+                <h1 style={{ textAlign: 'center', color: '#F4E1FC', fontWeight: 'bold' ,paddingTop:'20px'}}>Beyond the Crest</h1>
                 <div className="button-container" style={{ textAlign: 'center' , marginTop: '20px' }}>
-                    <button className="student-login"  style={{ backgroundColor: '#4C464F', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#FFFFFF' }}>Home</button>
+                    <button className="student-login"  style={{ backgroundColor: '#4C464F', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#E3BAF5' }}>Home</button>
                     <a href='/login'><button  className="faculty-login"  style={{ backgroundColor: '#4C464F', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#E3BAF5' }}>Login</button></a>
-                    <a href='/About'><button  className="about-us"  style={{ backgroundColor:'#4C464F', marginRight: '10px', padding: '10px 10px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>About Us</button></a>
-                    <button className="home"  style={{ backgroundColor: '#4C464F', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>More</button>
+                    <a href='/About'><button  className="about-us"  style={{ backgroundColor:'#4C464F', marginRight: '10px', padding: '10px 10px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#E3BAF5' }}>About Us</button></a>
+                    <button className="home"  style={{ backgroundColor: '#4C464F', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#E3BAF5' }}>More</button>
                 </div>
             </div>
         );
