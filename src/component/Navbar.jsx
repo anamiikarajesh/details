00e9962490f9af9e25180a965c02cd8dca36c6a8
anamiikarajesh <158269912+anamiikarajesh@users.noreply.github.com>
2024-07-03 17:06:45 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -8,7 +8,7 @@ class LoginPage extends React.Component {
         return (
             <div className="container" style={{backgroundImage:'url(main.jpg)',height:'100vh',backgroundSize:'cover'}}>
                 <h1 style={{ textAlign: 'center', color: '#F4E1FC', fontWeight: 'bold' ,paddingTop:'20px'}}>Beyond the Crest</h1>
-                <div className="button-container" style={{ textAlign: 'center' , marginTop: '20px' }}>
+                <div className="button-container" style={{ textAlign: 'center' , marginTop: '20px',color:'#212021' }}>
                     <button className="student-login"  style={{ backgroundColor: '#4C464F', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#E3BAF5' }}>Home</button>
                     <a href='/login'><button  className="faculty-login"  style={{ backgroundColor: '#4C464F', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#E3BAF5' }}>Login</button></a>
                     <a href='/About'><button  className="about-us"  style={{ backgroundColor:'#4C464F', marginRight: '10px', padding: '10px 10px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#E3BAF5' }}>About Us</button></a>
