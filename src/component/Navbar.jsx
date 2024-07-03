3cf4912a0050161e6ccafd38c26c90101ee2dd3a
anamiikarajesh <158269912+anamiikarajesh@users.noreply.github.com>
2024-07-03 17:09:34 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -10,9 +10,9 @@ class LoginPage extends React.Component {
                 <h1 style={{ textAlign: 'center', color: '#F4E1FC', fontWeight: 'bold' ,paddingTop:'20px'}}>Beyond the Crest</h1>
                 <div className="button-container" style={{ textAlign: 'center' , marginTop: '20px' }}>
                     <button className="student-login"  style={{ backgroundColor: '#212021', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#E3BAF5' }}>Home</button>
-                    <a href='/login'><button  className="faculty-login"  style={{ backgroundColor: '#4C464F', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#E3BAF5' }}>Login</button></a>
-                    <a href='/About'><button  className="about-us"  style={{ backgroundColor:'#4C464F', marginRight: '10px', padding: '10px 10px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#E3BAF5' }}>About Us</button></a>
-                    <button className="home"  style={{ backgroundColor: '#4C464F', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#E3BAF5' }}>More</button>
+                    <a href='/login'><button  className="faculty-login"  style={{ backgroundColor: '#212021', marginRight: '10px', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#E3BAF5' }}>Login</button></a>
+                    <a href='/About'><button  className="about-us"  style={{ backgroundColor:'#212021', marginRight: '10px', padding: '10px 10px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#E3BAF5' }}>About Us</button></a>
+                    <button className="home"  style={{ backgroundColor: '#212021', padding: '10px 20px', border: 'none', borderRadius: '5px', cursor: 'pointer',color:'#E3BAF5' }}>More</button>
                 </div>
             </div>
         );
